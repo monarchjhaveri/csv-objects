7860edcf39bb7345b7431e9458d734706ad30746
Monarch <monarchwadia@gmail.com>
2016-02-26 13:53:23 -0500

    Fixed array parsing spec.

--- spec/array-parsing-spec.js
+++ spec/array-parsing-spec.js
@@ -22,23 +22,26 @@ describe("Simple parsing", function() {
 
     it("reads a simple file and outputs expected simple object", function(done) {
         expect(this.parsedObject).toEqual(jasmine.objectContaining([
-            sample("Alpha", "Beta", "Omega"),
-            sample("One", "Two", "Three"),
-            sample("Parent", "Child", "Grandchild")
+            sample("Alpha", 100, "Beta", 200, "Omega", 300),
+            sample("One", 1, "Two", 2, "Three", 3),
+            sample("Parent", 63, "Child", 43, "Grandchild", 23)
         ]));
         done();
     });
 });
 
-function sample(name1, name2, name3) {
+function sample(name1, num1, name2, num2, name3, num3) {
     return {
         name: name1,
+        number: num1,
         children: [
             {
                 name: name2,
+                number: num2,
                 children: [
                     {
-                        name: name3
+                        name: name3,
+                        number: num3
                     }
                 ]
             }
